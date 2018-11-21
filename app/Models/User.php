56db914945686e1d9f36552f99f2307fd387a4af
vagrant <vagrant@homestead>
2018-11-21 01:39:26 +0000

    demo

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -9,9 +9,11 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    protected $table = 'users';
+
     /**
      * The attributes that are mass assignable.
-     *
+     * fillable 过滤用户提交的字段，只有包含在该属性中的字段才能够被正常更新
      * @var array
      */
     protected $fillable = [
